66e0e1820e44725e4ae4805823185d021a620598
arizfaiyaz <arizfaiyazwork@gmail.com>
2025-08-27 01:41:42 +0530

    Add CORS and nanoid dependencies; implement URL creation endpoint in Express server

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,23 @@
 import express from "express";
-
+import cors from "cors";
+import { nanoid } from "nanoid";
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/api/create', (req, res) => {
+    const url = req.body;
+    console.url(url);
+    res.send(nanoid(7));
+});
+
+
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
 });
+
+// GET - Redirection
+// POST - Create Short URL
